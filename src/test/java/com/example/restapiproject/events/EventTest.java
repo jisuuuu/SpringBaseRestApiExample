ce5fae6ab6f64e78035109d331c09715bfe01c18
Jisu Choi <gjkl55@naver.com>
2022-08-16 20:19:19 +0900

    이벤트 도메인 구현 -> annotation의 필요성

---
+++ src/test/java/com/example/restapiproject/events/EventTest.java
@@ -0,0 +1,35 @@
+package com.example.restapiproject.events;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class EventTest {
+
+    @Test
+    public void builder() {
+        Event event = Event.builder()
+                .name("Inflern Spring Rest API")
+                .description("Hi I'm Jisu")
+                .build();
+        assertThat(event).isNotNull();
+    }
+
+    @Test
+    public void javaBean() {
+
+        //Given
+        String name = "Event";
+        String description = "Spring";
+
+        //When
+        Event event = new Event();
+        event.setName(name);
+        event.setDescription(description);
+
+        //Then
+        assertThat(event.getName()).isEqualTo(name);
+        assertThat(event.getDescription()).isEqualTo(description);
+    }
+
+}
