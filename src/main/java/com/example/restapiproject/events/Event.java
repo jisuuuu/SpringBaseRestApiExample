4b3de22ce7cf8be0a359717f59ec96235ebe6f9b
Jisu Choi <gjkl55@naver.com>
2022-08-19 21:20:47 +0900

    이벤트 API 테스트 클래스 생성 > 완성 => 201 응답 받기

--- src/main/java/com/example/restapiproject/events/Event.java
+++ src/main/java/com/example/restapiproject/events/Event.java
@@ -7,6 +7,7 @@ import java.time.LocalDateTime;
 @Builder @AllArgsConstructor @NoArgsConstructor
 @Getter @Setter @EqualsAndHashCode(of = "id")
 public class Event {
+    private Integer id;
     private String name;
     private String description;
     private LocalDateTime beginEnrollmentDateTime;
--- src/test/java/com/example/restapiproject/events/EventControllerTests.java
+++ src/test/java/com/example/restapiproject/events/EventControllerTests.java
@@ -1,5 +1,6 @@
 package com.example.restapiproject.events;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,9 +9,13 @@ import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+
+import java.time.LocalDateTime;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest
@@ -19,13 +24,32 @@ public class EventControllerTests {
     @Autowired
     MockMvc mockMvc;
 
+    @Autowired
+    ObjectMapper objectMapper;
+
     // api.json api.xml
     @Test
     public void createEvent() throws Exception {
-        mockMvc.perform(post("/api/events")
+
+        Event event = Event.builder()
+                .name("Spring")
+                .description("Rest API Development with Spring")
+                .beginEnrollmentDateTime(LocalDateTime.of(2022, 8, 19, 21, 0, 0))
+                .closeEnrollmentDateTime(LocalDateTime.of(2022, 8, 19, 21, 0, 0))
+                .beginEventDateTime(LocalDateTime.of(2022, 8, 19, 21, 0, 0))
+                .endEventDateTime(LocalDateTime.of(2022, 8, 19, 21, 0, 0))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("강남역")
+                .build();
+
+        mockMvc.perform(post("/api/events/")
                         .contentType(MediaType.APPLICATION_JSON_UTF8)
                         .accept(MediaTypes.HAL_JSON)
-                )
-                .andExpect(status().isCreated());
+                        .content(objectMapper.writeValueAsString(event)))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("id").exists());
     }
 }
